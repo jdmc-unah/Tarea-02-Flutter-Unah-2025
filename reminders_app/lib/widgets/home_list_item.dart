29ba1fa768fc4f15d86db96c5b0add9970380481
Daniel Mejia <daniel.cuellar@unah.hn>
2025-06-19 16:05:51 -0600

    Widget personalizado para elementos del listview

---
+++ reminders_app/lib/widgets/home_list_item.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class HomeListItem extends StatelessWidget {
+  const HomeListItem({
+    super.key,
+    required this.title,
+    required this.subtitle,
+    required this.icon,
+    this.iconColor,
+  });
+
+  final String title;
+  final IconData icon;
+  final String subtitle;
+  final iconColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Icon(icon, size: 40, color: iconColor),
+      title: Text(
+        title,
+        style: TextStyle(
+          fontSize: 17.5,
+          color: Colors.grey[1000],
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      subtitle: Text(
+        subtitle,
+        style: TextStyle(fontSize: 17, color: Colors.grey[600]),
+      ),
+    );
+  }
+}
